fd55ae232d60ae1c4e7b757b5da5bf6aa6931789
Alexander Ormseth <a_ormseth@hotmail.com>
2022-08-03 22:19:31 +0200

    remove client

--- client/src/features/codeFragment/FrontPage.tsx
+++
@@ -1,6 +0,0 @@
-import AddCodeFragmentForm from "./form/AddCodeFragmentForm";
-
-// just show the form on the front-page
-const FrontPage = () => <AddCodeFragmentForm/>;
-
-export default FrontPage;
